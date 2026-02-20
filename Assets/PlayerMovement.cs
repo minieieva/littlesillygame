456f4092ae7bd8407330ebc10119e40220d9d631
LILIA <bellahcenlilia@gmail.com>
2026-02-20 10:12:19 +0100

    crid based movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private Tilemap Tilemap;
+    private Vector2 InitialInput;
+    private Vector2 direction;
+
+    public void OnMove(InputValue input)
+    {
+        InitialInput = input.Get<Vector2>();
+
+        direction = Cardinal(InitialInput);
+
+        if (direction != Vector2.zero)
+        {
+            Forward(direction);
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    private void Forward(Vector2 direction)
+    {
+        Vector3Int Cell = Tilemap.WorldToCell(transform.position);
+        Vector3Int TargetPosition = Cell + new Vector3Int((int)direction.x, (int)direction.y, 0);
+
+        // if (!Tilemap.HasTile(TargetPosition))
+        //     return;
+        BoundsInt bounds = Tilemap.cellBounds;
+
+        if (!bounds.Contains(TargetPosition))
+            return;
+
+        Vector2 FinalPosition = Tilemap.GetCellCenterWorld(TargetPosition);
+        transform.position = FinalPosition;
+    }
+
+    private Vector2 Cardinal(Vector2 InitialInput)
+    {
+        if(Math.Abs(InitialInput.x) < Math.Abs(InitialInput.y))
+        {
+            return new Vector2(0,Math.Sign(InitialInput.y));
+        }
+        if(Math.Abs(InitialInput.x) > Math.Abs(InitialInput.y))
+        {
+            return new Vector2(Math.Sign(InitialInput.x), 0);
+        }
+        return Vector2.zero;
+
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scenes/Scripts/PlayerMovement.cs
+++ Assets/Scenes/Scripts/PlayerMovement.cs
@@ -1,42 +1,42 @@
-using UnityEngine;
-using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
-using System.Collections;
-using UnityEngine.Tilemaps;
+// using UnityEngine;
+// using UnityEngine.InputSystem;
+// using UnityEngine.SceneManagement;
+// using System.Collections;
+// using UnityEngine.Tilemaps;
 
-[RequireComponent(typeof(Rigidbody2D))]
+// [RequireComponent(typeof(Rigidbody2D))]
 
-public class PlayerMovement : MonoBehaviour {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private Tilemap grid; 
+// public class PlayerMovement : MonoBehaviour {
+//     [SerializeField] private float moveSpeed = 5f;
+//     [SerializeField] private Tilemap grid; 
 
-    private Rigidbody2D rb;
-    private Vector2 movement;
+//     private Rigidbody2D rb;
+//     private Vector2 movement;
 
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+//     private void Awake()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//     }
 
-    public void OnMove(InputValue input)
-    {
-        movement = input.Get<Vector2>();
+//     public void OnMove(InputValue input)
+//     {
+//         movement = input.Get<Vector2>();
 
-        Vector3Int position = new Vector3Int(
-            (int)Mathf.Round(movement.x),
-            (int)Mathf.Round(movement.y),
-            0
-        );
+//         Vector3Int position = new Vector3Int(
+//             (int)Mathf.Round(movement.x),
+//             (int)Mathf.Round(movement.y),
+//             0
+//         );
 
-        Move(position);
-    }
+//         Move(position);
+//     }
 
-    private void Move(Vector3Int direction)
-    {
-        Vector3Int currentCell = grid.WorldToCell(transform.position);
+//     private void Move(Vector3Int direction)
+//     {
+//         Vector3Int currentCell = grid.WorldToCell(transform.position);
 
-        Vector3Int targetCell = currentCell + direction;
+//         Vector3Int targetCell = currentCell + direction;
 
-        transform.position = grid.GetCellCenterWorld(targetCell);
-    }
-}
+//         transform.position = grid.GetCellCenterWorld(targetCell);
+//     }
+// }
